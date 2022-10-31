c8edf3ddfe6b30cc52ab77112b6c44ffd024cbf1
Nick Pattison <pattison.nick@gmail.com>
2022-10-31 14:16:42 -0400

    Add setup file

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   testEnvironment: 'jsdom',
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles:['<rootDir>/tests/setup.ts'],
 };
