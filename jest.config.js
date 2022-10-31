be0823c6558c218c19759997b4d01cc86a384aaa
Nick Pattison <pattison.nick@gmail.com>
2022-10-31 15:06:33 -0400

    Allow jest to test JS files
    Added js to testRegex value in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset:'ts-jest',
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'jsdom',
-  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles:['<rootDir>/tests/setup.ts'],
 };
