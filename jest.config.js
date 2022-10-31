b0d17e16210ddadfc67407abf3313716119f9dad
Nick Pattison <pattison.nick@gmail.com>
2022-10-31 13:52:06 -0400

    Add npm, jest, typescript settings; add .gitignore
    + .gitignore
    + jest config
    + package.json + package-lock.json
    + tsconfig.json

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset:'ts-jest',
+  transform: {'^.+\\.ts?$': 'ts-jest'},
+  testEnvironment: 'jsdom',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
